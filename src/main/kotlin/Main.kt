6126b154a96a5f397a59de78ca1b7ed4ac3f92ef
Vladi71 <ronrs6@mail.ru>
2020-09-10 21:47:37 +0300

    Commission Int

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,43 +2,42 @@ package ru.netology
 
 
 fun main() {
-    println(transfer(0, 3000, "visa and Mir"))
+    println(transfer(0, 900000, "mastercard and Maestro"))
 
 }
 
 fun transfer(total: Int, amount: Int, transferMethod: String): String {
 
     return when (transferMethod) {
-        "mastercard and Maestro" -> mastercardAndMaestro(total, amount)
-        "visa and Mir" -> visaAndMir(total, amount)
-        "vkPay" -> vkPay(total, amount)
+        "mastercard and Maestro" -> mastercardAndMaestro(total, amount).toString()
+        "visa and Mir" -> visaAndMir(total, amount).toString()
+        "vkPay" -> vkPay(total, amount).toString()
         else -> "Данный счет не существует!"
     }
 }
 
-fun mastercardAndMaestro(total: Int, amount: Int): String {
+fun mastercardAndMaestro(total: Int, amount: Int): Int {
     val masterCommission = 0.0060
     val commission: Int = (amount * masterCommission + 2000).toInt()
-    return when {
-        total + amount <= 7_500_000 || total + amount >= 300_00 -> "Перевод выполнен! Комиссия составила 0 коп."
-        else -> "Перевод выполнен! Комиссия составила $commission коп."
+    val noCommission = 0
+
+    return  when {
+        total + amount <= 75_000_00  -> noCommission
+        else -> commission
     }
+
 }
 
-fun visaAndMir(total: Int, amount: Int): String {
+fun visaAndMir(total: Int, amount: Int): Int {
     val minCommission = 3500
     val percentCommission = 0.0075
     val commission: Int = (amount * percentCommission).toInt()
     return when {
-        commission < 3500 -> "Перевод выполнен! Комиссия составила $minCommission коп."
-        else -> "Перевод выполнен! Комиссия составила $commission коп."
+        commission < 3500 -> minCommission
+        else -> commission
     }
 }
 
-fun vkPay(total: Int, amount: Int): String {
-    return when {
-        total > 40_000_00 -> "Превышен лимит переводов в месяц!"
-        amount > 15_000_00 -> "Превышена максимальная сумма перевода!"
-        else -> "Перевод выполнен! Комиссия составила 0 коп."
-    }
+fun vkPay(total: Int, amount: Int): Int {
+    return 0
 }
